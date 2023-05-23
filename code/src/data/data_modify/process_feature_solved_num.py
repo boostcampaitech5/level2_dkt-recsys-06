db40938626fe1d34a60855eaa86a201b065f6857
root <junhyuk1229@gmail.com>
2023-05-23 11:43:20 +0000

    [Column] Added solved number column
    Signed-off-by: root <junhyuk1229@gmail.com>

---
+++ code/src/data/data_modify/process_feature_solved_num.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+
+def create_feature_test_solved_num(data: pd.DataFrame) -> None:
+    data["train"]["test_solved_num"] = (
+        data["train"]
+        .sort_values(["timestamp"])
+        .groupby(["user_id", "test_numeric_id"])
+        .cumcount()
+    )
+    data["test"]["test_solved_num"] = (
+        data["train"]
+        .sort_values(["timestamp"])
+        .groupby(["user_id", "test_numeric_id"])
+        .cumcount()
+    )
+
+    return
+
+
+def create_feature_user_solved_num(data: pd.DataFrame) -> None:
+    data["train"]["user_solved_num"] = (
+        data["train"].sort_values(["timestamp"]).groupby(["user_id"]).cumcount()
+    )
+    data["test"]["user_solved_num"] = (
+        data["train"].sort_values(["timestamp"]).groupby(["user_id"]).cumcount()
+    )
+
+    return
